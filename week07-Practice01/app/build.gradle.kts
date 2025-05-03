d78c3441577802ad1ff13dbf4e230a0f08ca2995
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2025-05-03 21:06:07 +0900

    complete

--- week07-Practice01/app/build.gradle.kts
+++ week07-Practice01/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ddwu.r.mobilesoft.week07.practice01"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ddwu.r.mobilesoft.week07.practice01"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    viewBinding {
+        enable = true
     }
 }
 
--- week07-Practice01/app/src/main/java/ddwu/r/mobilesoft/week07/practice01/MainActivity.kt
+++ week07-Practice01/app/src/main/java/ddwu/r/mobilesoft/week07/practice01/MainActivity.kt
@@ -1,20 +1,75 @@
 package ddwu.r.mobilesoft.week07.practice01
 
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ddwu.r.mobilesoft.week07.practice01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActicityTAG"
+    lateinit var viewBinding: ActivityMainBinding
+    var SoupSelected01 = false
+    var SoupSelected02 = false
+    var NoodleSelected01 = false
+    var NoodleSelected02 = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_foods, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.SoupItem01 -> {
+                Log.i(TAG, "김치찌개 맛있어요!")
+                SoupSelected01 = !SoupSelected01
+                true
+            }
+            R.id.SoupItem02 -> {
+                Log.i(TAG, "순두부찌개 맛있어요!")
+                SoupSelected02 = !SoupSelected02
+                true
+            }
+            R.id.NoodleItem01 -> {
+                Log.i(TAG, "짜장면 맛있어요!")
+                NoodleSelected01 = !NoodleSelected01
+                true
+            }
+            R.id.NoodleItem02 -> {
+                Log.i(TAG, "파스타 맛있어요!")
+                NoodleSelected02 = !NoodleSelected02
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        val soupItemCheck01: MenuItem? = menu?.findItem(R.id.SoupItem01)
+        soupItemCheck01?.isChecked = SoupSelected01
+
+        val soupItemCheck02: MenuItem? = menu?.findItem(R.id.SoupItem02)
+        soupItemCheck02?.isChecked = SoupSelected01
+
+        val noodleItemCheck01: MenuItem? = menu?.findItem(R.id.NoodleItem01)
+        noodleItemCheck01?.isChecked = NoodleSelected01
+
+        val noodleItemCheck02: MenuItem? = menu?.findItem(R.id.NoodleItem02)
+        noodleItemCheck02?.isChecked = NoodleSelected02
+
+        return true
     }
 }
