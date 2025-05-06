675f031519d7cb96b99b6da28330f2c9b606ae57
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2025-05-06 19:20:58 +0900

    week09-Lecture complete

---
+++ week09-RecyclerViewTest/app/src/main/java/ddwu/r/mobilesoft/week09/recyclerviewtest/ItemAdapter.kt
@@ -0,0 +1,34 @@
+package ddwu.r.mobilesoft.week09.recyclerviewtest
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(val context: Context, val list: ArrayList<String>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.item_layout, parent, false)
+        return ItemViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.tvItemText.text = list[position]
+    }
+
+    class  ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val TAG = "ItemViewHolder"
+        val tvItemText : TextView = view.findViewById<TextView>(R.id.tvItemText)
+        init {
+            tvItemText.setOnClickListener {
+                Log.i(TAG, "tvItemTextClick! $adapterPosition")
+            }
+        }
+    }
+}
