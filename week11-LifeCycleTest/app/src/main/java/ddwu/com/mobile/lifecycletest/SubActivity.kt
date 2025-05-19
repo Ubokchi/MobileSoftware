bc2845cb624755e3c853cef291e5a6b7d8481e78
Ubokchi <105459234+Ubokchi@users.noreply.github.com>
2025-05-19 10:47:48 +0900

    week11-LifeCycleTest

---
+++ week11-LifeCycleTest/app/src/main/java/ddwu/com/mobile/lifecycletest/SubActivity.kt
@@ -0,0 +1,65 @@
+package ddwu.com.mobile.lifecycletest
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import ddwu.com.mobile.lifecycletest.databinding.ActivitySubBinding
+
+class SubActivity : AppCompatActivity() {
+
+    val TAG = "Child"
+    lateinit var subBinding : ActivitySubBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        subBinding = ActivitySubBinding.inflate(layoutInflater)
+        setContentView(subBinding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        subBinding.subTextView.setOnClickListener{
+            finish()
+        }
+        Log.v(TAG, "$TAG - onCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(TAG, "$TAG - onStart()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(TAG, "$TAG - onRestart()")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(TAG, "$TAG - onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(TAG, "$TAG - onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(TAG, "$TAG - onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG, "$TAG - onDestroy()")
+    }
+}
